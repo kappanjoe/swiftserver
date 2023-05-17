d9e83deb139202cbd906e1fc607f54bc07ba0c6f
Joe Van Alstyne <6261485+kappanjoe@users.noreply.github.com>
2023-05-17 20:43:37 +0900

    feat: add /hello/:name endpoint

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,9 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+	
+	app.get("hello", ":name") { req -> String in
+		let name = req.parameters.get("name")!
+		return "Hello, \(name)!"
+	}
 }
